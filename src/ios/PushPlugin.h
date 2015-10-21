55551b3755592ab529d45c44b13dede293811522
zss0512 <310207614@qq.com>
2015-10-21 17:37:57 +0800

    Update PushPlugin.h

--- src/ios/PushPlugin.h
+++ src/ios/PushPlugin.h
@@ -29,27 +29,19 @@
 
 @interface PushPlugin : CDVPlugin
 {
-    NSDictionary *notificationMessage;
-    BOOL    isInline;
-    NSString *notificationCallbackId;
+
     NSString *callback;
     
     BOOL ready;
 }
 
 @property (nonatomic, copy) NSString *callbackId;
-@property (nonatomic, copy) NSString *notificationCallbackId;
+
 @property (nonatomic, copy) NSString *callback;
 
-@property (nonatomic, strong) NSDictionary *notificationMessage;
-@property BOOL                          isInline;
 
 - (void)registePush:(CDVInvokedUrlCommand*)command;
 
-- (void)didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
-- (void)didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
-
-- (void)setNotificationMessage:(NSDictionary *)notification;
-- (void)notificationReceived;
+-(void)successWithMessage:(NSString *)message;
 
 @end
