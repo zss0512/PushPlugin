9c681052da5617ddd1893a6aafc10081a15395c2
zss0512 <310207614@qq.com>
2015-10-21 17:36:51 +0800

    Update AppDelegate+notification.h

--- src/ios/AppDelegate+notification.h
+++ src/ios/AppDelegate+notification.h
@@ -13,8 +13,5 @@
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo;
 - (void)applicationDidBecomeActive:(UIApplication *)application;
-- (id) getCommandInstance:(NSString*)className;
-
-@property (nonatomic, retain) NSDictionary	*launchNotification;
 
 @end
