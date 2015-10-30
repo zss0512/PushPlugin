3a94f2851c5c02d814040e7afddbca6547628751
zss0512 <310207614@qq.com>
2015-10-30 09:34:09 +0800

    Update PushPlugin.m

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -1,27 +1,10 @@
-/*
- Copyright 2009-2011 Urban Airship Inc. All rights reserved.
-
- Redistribution and use in source and binary forms, with or without
- modification, are permitted provided that the following conditions are met:
-
- 1. Redistributions of source code must retain the above copyright notice, this
- list of conditions and the following disclaimer.
-
- 2. Redistributions in binaryform must reproduce the above copyright notice,
- this list of conditions and the following disclaimer in the documentation
- and/or other materials provided withthe distribution.
-
- THIS SOFTWARE IS PROVIDED BY THE URBAN AIRSHIP INC``AS IS'' AND ANY EXPRESS OR
- IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
- EVENT SHALL URBAN AIRSHIP INC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
- INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
- OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+//
+//  PushPlugin.m
+//  ChaoGeDoor
+//
+//  Created by jwt on 15/10/8.
+//
+//
 
 #import "PushPlugin.h"
 #import "IXTNotification.h"
@@ -31,8 +14,102 @@
 
 @implementation PushPlugin
 
+@synthesize webView;
+@synthesize callback;
 @synthesize callbackId;
+@synthesize notificationMessage;
 
+
+- (CDVPlugin*)initWithWebView:(UIWebView*)theWebView{
+    if (self=[super initWithWebView:theWebView]) {
+        
+        NSNotificationCenter *defaultCenter = [NSNotificationCenter defaultCenter];
+//        [defaultCenter addObserver:self
+//                          selector:@selector(networkDidReceiveMessage:)
+//                              name:kJPFNetworkDidReceiveMessageNotification
+//                            object:nil];
+        
+        [defaultCenter addObserver:self
+                          selector:@selector(networkDidReceiveNotification:)
+                              name:iPushPluginReceiveNotificaiton
+                            object:nil];
+        
+    }
+    return self;
+}
+//- (void)networkDidReceiveMessage:(NSNotification *)notification {
+//    
+//    NSDictionary *userInfo = [notification userInfo];
+//    NSLog(@"%@",userInfo);
+//    
+//    NSError  *error;
+//    NSData   *jsonData   = [NSJSONSerialization dataWithJSONObject:userInfo options:0 error:&error];
+//    NSString *jsonString = [[NSString alloc]initWithData:jsonData encoding:NSUTF8StringEncoding];
+//    
+//    NSLog(@"%@",jsonString);
+//    
+//    dispatch_async(dispatch_get_main_queue(), ^{
+//        
+//        [self writeJavascript:[NSString stringWithFormat:@"window.aiXinPushServer.registeAixinPush('%@')",jsonString]];
+//        
+//    });
+//    
+//}
+
+- (void)networkDidReceiveNotification:(NSNotification *)notification{
+    notificationMessage = [notification object];
+    NSLog(@"networkDidReceiveNotification:%@",notification);
+    //here add your  ode
+    NSDictionary *object = [notification object];
+    NSString* alertStr = nil;
+    if (callbackId) {
+        NSLog(@"%@",callbackId);
+    }
+//    NSError  *error;
+//    NSData   *jsonData   = [NSJSONSerialization dataWithJSONObject:object options:0 error:&error];
+//    NSString *jsonString = [[NSString alloc]initWithData:jsonData encoding:NSUTF8StringEncoding];
+    alertStr = [[object objectForKey:@"aps"]objectForKey:@"alert"];
+    if ([UIApplication sharedApplication].applicationState == UIApplicationStateActive && [alertStr isEqualToString:@"你的账号已在别处登陆，如不是本人操作，请确认此消息"]){
+        [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
+        UIAlertView*alertView = [[UIAlertView alloc]initWithTitle:@"下线通知" message:alertStr delegate:self cancelButtonTitle:@"退出" otherButtonTitles:@"重新登陆", nil];
+        [alertView show];
+        NSLog(@"state%ld",(long)[UIApplication sharedApplication].applicationState);
+    }else{
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [UIApplication sharedApplication].applicationIconBadgeNumber -=1;
+            NSMutableDictionary *pushClick = [NSMutableDictionary dictionary];
+            NSString *classType = [[notificationMessage objectForKey:@"extra" ] objectForKey:@"classtype"];
+            NSString *detailId = [[notificationMessage objectForKey:@"extra" ] objectForKey:@"id"];
+            [UIApplication sharedApplication].applicationIconBadgeNumber -=1;
+            [pushClick setValue:@"notifyclick" forKey:@"type"];
+            [pushClick setValue:classType forKey:@"classType"];
+            [pushClick setValue:detailId forKey:@"detailId"];
+            NSLog(@"pushClick:%@",pushClick);
+            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:pushClick options:NSJSONWritingPrettyPrinted error:nil];
+            NSString *stringClick = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+            [self successWithMessage:[NSString stringWithFormat:@"%@",stringClick]];
+        });
+    }
+    
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSLog(@"buttonIndex:%ld", (long)buttonIndex);
+    if (buttonIndex == 1) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+
+            NSMutableDictionary *reloginClick = [NSMutableDictionary dictionary];
+            [UIApplication sharedApplication].applicationIconBadgeNumber -=1;
+            [reloginClick setValue:@"relogin" forKey:@"type"];
+            NSLog(@"reloginClick:%@",reloginClick);
+            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:reloginClick options:NSJSONWritingPrettyPrinted error:nil];
+            NSString *stringClick = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+            [self successWithMessage:[NSString stringWithFormat:@"%@",stringClick]];
+        });
+    }
+}
+//注销推送
 - (void)unregistePush:(CDVInvokedUrlCommand*)command;
 {
 	self.callbackId = command.callbackId;
@@ -40,10 +117,11 @@
     [[UIApplication sharedApplication] unregisterForRemoteNotifications];
     [self successWithMessage:@"unregistered"];
 }
-
+//注册推送
 - (void)registePush:(CDVInvokedUrlCommand*)command;
 {
-	self.callbackId = command.callbackId;
+    
+    self.callbackId = command.callbackId;
 	NSLog(@"%@",callbackId);
     NSUserDefaults *userDefault=[NSUserDefaults standardUserDefaults];
     NSData *token=[userDefault objectForKey:@"deviceToken"];
@@ -53,11 +131,11 @@
     NSString *plistPath = [[NSBundle mainBundle]pathForResource:@"PushConfig" ofType:@"plist"];
     NSMutableDictionary *data = [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
     NSString *iappKey = [data objectForKey:@"AppKey"];
-    NSString *isecretkey = [data objectForKey:@"SecretKey"];
-    NSLog(@"%@,%@",iappKey,isecretkey);
+    NSString *isecretKey = [data objectForKey:@"SecretKey"];
+    NSLog(@"%@,%@",iappKey,isecretKey);
     IXTNotification *ixtNotification = [[IXTNotification alloc] init: (iappKey).intValue andToken:token delegate:nil];
     //delegate是http的回调,可参照HttpDelegate.h实现
-    BOOL flag = [ixtNotification register:isecretkey flag:true];
+    BOOL flag = [ixtNotification register:isecretKey flag:true];
     NSString *pushToken = [[[[token description]
                              
                              stringByReplacingOccurrencesOfString:@"<" withString:@""]
@@ -77,15 +155,21 @@
     //NSError *parseError = nil;
     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:resultToken options:NSJSONWritingPrettyPrinted error:nil];
     NSString *resultString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-    NSLog(@"注册通知后返回给js的字符串：%@",resultString);
     
+    NSLog(@"注册通知后返回给js的字符串：%@",resultString);
+//    //获取当前app版本号version
+//    NSString* version = [NSString stringWithFormat:@"%@(%@)", [[[NSBundle mainBundle] infoDictionary] valueForKey:@"CFBundleShortVersionString"] ,[[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString*)kCFBundleVersionKey]];
+   // msg = '{"type":"promit","msg":"你的账号已在别处登陆，如不是本人操作，请确认此消息"}';
     [self successWithMessage:[NSString stringWithFormat:@"%@",resultString]];
-	
+//    NSString *str = [self.webview stringByEvaluatingJavaScriptFromString:@"log_in()"];
+//    NSLog(@"JS返回值：%@",str);
+
 }
 
 -(void)successWithMessage:(NSString *)message
 {
     CDVPluginResult *commandResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:message];
+    [commandResult setKeepCallbackAsBool:TRUE];
     
     [self.commandDelegate sendPluginResult:commandResult callbackId:self.callbackId];
 }
@@ -163,6 +247,13 @@
     free(msgBuffer);
     
     return macAddressString;
+}
+
+- (void)dealloc{
+    //移除指定的通知，不然会造成内存泄露
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:@"iPushPluginReceiveNotificaiton" object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 @end
